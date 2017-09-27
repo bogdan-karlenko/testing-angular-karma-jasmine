c840da2b0451a08ad0024e9e64c97ddff1e9cb1c
Bogdan Karlenko <opposite.ps@gmail.com>
2017-09-27 11:51:16 +0300

    End of Part 1

--- app/services/users/users.js
+++ app/services/users/users.js
@@ -4,12 +4,46 @@
   // Creating the module and factory we referenced in the beforeEach blocks in our test file
   angular.module('api.users', [])
     .factory('Users', function() {
-      var Users = {
-        a: "a",
-        b: "b",
+      var Users = {};
+
+      var userList = [{
+        id: '1',
+        name: 'Jane',
+        role: 'Designer',
+        location: 'New York',
+        twitter: 'gijane'
+      }, {
+        id: '2',
+        name: 'Bob',
+        role: 'Developer',
+        location: 'New York',
+        twitter: 'billybob'
+      }, {
+        id: '3',
+        name: 'Jim',
+        role: 'Developer',
+        location: 'Chicago',
+        twitter: 'jimbo'
+      }, {
+        id: '4',
+        name: 'Bill',
+        role: 'Designer',
+        location: 'LA',
+        twitter: 'dabill'
+      }];
+
+      Users.all = function() {
+        return userList;
       };
 
-      // Users.method = function() {};
+      Users.findById = function(id) {
+
+        var singleUser = userList.find(function(user) {
+          return user.id === id
+        });
+
+        return singleUser;
+      };
 
       return Users;
     });
--- app/services/users/users.spec.js
+++ app/services/users/users.spec.js
@@ -1,6 +1,40 @@
 describe('User control module', function() {
   var Users;
 
+  var userList = [{
+    id: '1',
+    name: 'Jane',
+    role: 'Designer',
+    location: 'New York',
+    twitter: 'gijane'
+  }, {
+    id: '2',
+    name: 'Bob',
+    role: 'Developer',
+    location: 'New York',
+    twitter: 'billybob'
+  }, {
+    id: '3',
+    name: 'Jim',
+    role: 'Developer',
+    location: 'Chicago',
+    twitter: 'jimbo'
+  }, {
+    id: '4',
+    name: 'Bill',
+    role: 'Designer',
+    location: 'LA',
+    twitter: 'dabill'
+  }];
+
+  var singleUser = {
+    id: '2',
+    name: 'Bob',
+    role: 'Developer',
+    location: 'New York',
+    twitter: 'billybob'
+  };
+
   beforeEach(angular.mock.module('api.users'));
 
   beforeEach(inject(function(_Users_) {
@@ -9,6 +43,32 @@ describe('User control module', function() {
 
   it('should exist', function() {
     expect(Users).toBeDefined();
+  });
+
+  describe('.all()', function() {
+
+    it('should exist', function() {
+      expect(Users.all).toBeDefined();
+    });
+
+    it('should return a hard-coded list of users', function() {
+      expect(Users.all()).toEqual(userList);
+    });
+  });
+
+  describe('.findById()', function() {
+
+    it('should exist', function() {
+      expect(Users.findById).toBeDefined();
+    });
+
+    it('should return one user object if it exists', function() {
+      expect(Users.findById('2')).toEqual(singleUser);
+    });
+
+    it('should return undefined if the user cannot be found', function() {
+      expect(Users.findById(NaN)).not.toBeDefined();
+    });
   });
 
 });
